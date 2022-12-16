d185898f1e55b4d8c969e03122cd4849c9f73863
Henry Wing Fung Yeung <88356994+Henkkk@users.noreply.github.com>
2022-12-16 12:15:10 +0800

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,21 @@
+#include <iostream>   // std::cout
+#include <string>     // std::string, std::to_string
+#include <typeinfo>
+
+class Solution {
+public:
+	bool isPalindrome(int x) {
+		int reversed;
+		string str;
+
+		str = to_string(x);
+		reverse(str.begin(), str.end());
+
+		if (to_string(x) == str) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+};
